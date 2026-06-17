c30483363972d2d0b7cc62d25b1d0830a1df9618
Bani Sond <banisond@MacBook-Pro-374.local>
2026-06-17 12:12:58 -0400

    Save Webcam Frames

--- docs/project_journal.md
+++ docs/project_journal.md
@@ -2,3 +2,17 @@ June 14
 Learned simle uses of pyttsx3
 Created first speaking prototype
 I asked ChatGPT to give me mini challenge projects to I could experiment with the package
+
+June 16
+Installed OpenCV
+I WAS ABLE TO DISPLAY FEED FROM MY WEBCAM!!
+Saved code for future reference
+Later I will need to access live feed from dog camera
+Tomorrow I will work on deliverable 3
+
+June 17
+I worked on createing a scalable way to collect data for later weeks
+I created a system that creates nesesary files and saves images in them
+Todo: 
+- program should ask user for input for which file to save data to
+- program needs to connect to dog cam, not webcam
--- scripts/webcam_test.py
+++ scripts/webcam_test.py
@@ -1,5 +1,23 @@
 import cv2
+from datetime import datetime
+from pathlib import Path
 
+#Create Folders
+folders = [
+    "data/outside",
+    "data/food",
+    "data/play",
+    "data/rest"
+]
+
+for folder in folders:
+    Path(folder).mkdir(parents=True, exist_ok=True)
+
+print("Folders Created")
+
+
+
+#Working with the Webcam
 camera = cv2.VideoCapture(0)
 
 while True:
@@ -9,10 +27,19 @@ while True:
         break
 
     cv2.imshow("Webcam Feed", frame)
+    #image = cv2.resize(frame, 224, 224)
+
 
     key = cv2.waitKey(1)
     if key in [27, ord('q')]:
+        print("Quit Feed")
         break
+    if key in [ord('s')]:
+        now = datetime.now()
+        stamp = f"{now:%Y%m%d_%H%M%S_%f}"
+        cv2.imwrite(f"data/play/test_image{stamp}.jpg", frame)
+        print(f"Saved {stamp}")
+        
     
     
 
